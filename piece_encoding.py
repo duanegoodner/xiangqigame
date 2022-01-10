f99d4d1afb25f2809be34c908234d9b10113566f
duanegoodner <55758382+duanegoodner@users.noreply.github.com>
2022-01-10 08:15:05 -0700

    Main (#1)
    * get_all_pieces_of() as dictionary comprehension
    
    * update gitignore
    
    * update gitignore
    
    * flying gen check works
    
    * add _move_rules dictionary
    
    * fix GampePiece.get_forward_direction()
    
    * add get_moves_it and get_untested_moves to move_rules module
    
    * commit after long hiatus from project
    
    * remove pycache
    
    * fix gitignore

--- piece_encoding.py
+++ piece_encoding.py
@@ -9,17 +9,22 @@ _code_to_color = {
 _color_to_code = dict((color, code) for code, color in _code_to_color.items())
 
 _code_to_type = {
-    'r': 'CHARIOT',
-    'h': 'HORSE',
-    'e': 'ELEPHANT',
-    'a': 'ADVISOR',
-    'g': 'GENERAL',
-    'c': 'CANNON',
-    's': 'SOLDIER',
+    'r': 'chariot',
+    'h': 'horse',
+    'e': 'elephant',
+    'a': 'advisor',
+    'g': 'general',
+    'c': 'cannon',
+    's': 'soldier',
     '-': 'NULL PIECE'
 }
 
 _type_to_code = dict((color, code) for code, color in _code_to_type.items())
+
+_forward_direction = {
+    'RED': 1,
+    'BLACK': -1
+}
 
 
 def decode_color(piece_code):
@@ -34,11 +39,9 @@ def decode_piece(piece_code):
     return _code_to_type[piece_code[0]], _code_to_color[piece_code[1]]
 
 
-def decode_board(encoded_board):
-    return [[decode_piece(encoded_board[row][col]) for col in
-            range(len(encoded_board[0]))] for row in
-            range(len(encoded_board))]
-
-
 def encode_piece(piece_type, color):
     return _type_to_code[piece_type] + _color_to_code[color]
+
+
+def get_forward_direction(color):
+    return _forward_direction[color]
