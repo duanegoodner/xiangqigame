7786cc58194ab42c6ebd66bb3126cd2600db4c76
Duane Goodner <dmgoodner@gmail.com>
2022-02-09 14:02:16 -0700

    replace 'open <relative path to json> with pkgutil.get_data()

--- xiangqigame/tests/game_scenarios.py
+++ xiangqigame/tests/game_scenarios.py
@@ -1,4 +1,5 @@
 import json
+import pkgutil
 from xiangqigame.common.enums import GameState
 from xiangqigame.game import Game
 from xiangqigame.data import alg_games as ag
@@ -6,38 +7,40 @@ from xiangqigame.user_io.single_move import convert_alg_move_list
 
 import unittest
 
-with open('../data/game_start.json') as game_info:
-    game_config = json.load(game_info)
-
 
 class GameResultTests(unittest.TestCase):
+    game_config = json.loads(
+        pkgutil.get_data('xiangqigame.data', 'game_start.json'))
+
     def test_a(self):
         moves_a = convert_alg_move_list(ag.game_a)
-        game_a = Game(game_config, auto_moves=moves_a)
+        game_a = Game(self.game_config, auto_moves=moves_a)
         game_a.play_auto_moves()
         self.assertTrue(game_a._game_state == GameState.RED_WON)
 
     def test_b(self):
         moves_b = convert_alg_move_list(ag.game_b)
-        game_b = Game(game_config, auto_moves=moves_b)
+        game_b = Game(self.game_config, auto_moves=moves_b)
         game_b.play_auto_moves()
         self.assertTrue(game_b._game_state == GameState.ILLEGAL_AUTO_MOVE)
 
     def test_c(self):
         moves_c = convert_alg_move_list(ag.game_c)
-        game_c = Game(game_config, auto_moves=moves_c)
+        game_c = Game(self.game_config, auto_moves=moves_c)
         game_c.play_auto_moves()
         self.assertTrue(game_c._game_state == GameState.UNFINISHED)
 
     def test_d(self):
         moves_d = convert_alg_move_list(ag.game_d)
-        game_d = Game(game_config, auto_moves=moves_d)
+        game_d = Game(self.game_config, auto_moves=moves_d)
         game_d.play_auto_moves()
         self.assertTrue(game_d._game_state == GameState.RED_WON)
 
 
 if __name__ == '__main__':
     unittest.main()
+
+
 
 
 
--- xiangqigame/tests/quick_game.py
+++ xiangqigame/tests/quick_game.py
@@ -1,10 +1,11 @@
 import json
+import pkgutil
 from xiangqigame.game import Game
 from xiangqigame.data import alg_games as ag
 from xiangqigame.user_io.single_move import convert_alg_move_list
 
-with open('../data/game_start.json') as game_info:
-    game_config = json.load(game_info)
+game_config = json.loads(
+        pkgutil.get_data('xiangqigame.data', 'game_start.json'))
 
 moves_d = convert_alg_move_list(ag.game_d)
 game_d = Game(game_config, auto_moves=moves_d)
