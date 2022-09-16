d424424cbebd2f215e3ecb0e27c15f9899881dc3
duanegoodner <55758382+duanegoodner@users.noreply.github.com>
2022-09-16 14:33:15 -0600

    Pkg restruct (#2)
    * add setup.cfg. keep minimal setup.py
    
    * convert to package

--- setup.py
+++ setup.py
@@ -1,17 +1,19 @@
-from distutils.core import setup
+# from distutils.core import setup
 import setuptools
 
+setuptools.setup()
 
-setup(
-    name='xiangqigame',
-    version='0.0.1',
-    description='A board game',
-    url='#',
-    author='duanegoodner',
-    install_requires=['numpy'],
-    author_email='dmgoodner@gmail.com',
-    packages=setuptools.find_packages(),
-    package_data={"": ["*.json"]},
-    include_package_data=True,
-    zip_safe=False
-)
+
+# setup(
+#     name='xiangqigame',
+#     version='0.0.1',
+#     description='A board game',
+#     url='#',
+#     author='duanegoodner',
+#     install_requires=['numpy'],
+#     author_email='dmgoodner@gmail.com',
+#     packages=setuptools.find_packages(),
+#     package_data={"": ["*.json"]},
+#     include_package_data=True,
+#     zip_safe=False
+# )
