4fec0616b099637a8a9ca7ea2b0747cd81b34f92
Duane Goodner <dmgoodner@gmail.com>
2022-11-12 22:28:59 -0700

    add conda_snapshots dir

--- build/lib.macosx-10.9-x86_64-cpython-310/xiangqigame/enums.py
+++
@@ -1,7 +0,0 @@
-from enum import Enum, auto
-
-
-class GameState(Enum):
-    UNFINISHED = auto()
-    RED_WON = auto()
-    BLACK_WON = auto()
--- build/lib.macosx-10.9-x86_64-cpython-310/xiangqigame/tests/all_c_board.py
+++
@@ -1,4 +0,0 @@
-import xiangqigame.cython_modules.game_board_c as gbc
-
-
-gbc.create_and_destroy_game_board_c()
