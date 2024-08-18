91bdfa97082ab44f45e649c81dfa1d7fd701304a
Duane Goodner <dmgoodner@gmail.com>
2024-08-18 16:19:54 +0000

    streamline Dockerfile and pip installation in Docker container

--- .devcontainer/entrypoint.sh
+++ .devcontainer/entrypoint.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
 
-cleanup() {
-    current_time=$(date "+%Y.%m.%d-%H.%M.%S")
-    conda env export -p $1 > $2/$current_time.environment.yml
-}
 
 trap 'true' SIGTERM
+trap 'true' SIGINT
 
-#Execute command
-# pip install ./project
+pip install ./project
 tail -f /dev/null &
-sudo service ssh start
-
-#Wait
 wait $!
 
-#Cleanup
-cleanup $1 $2
+echo closing
