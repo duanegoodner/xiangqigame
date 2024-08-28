1db67631ba5f7585e4c6287a76a75df0d1409e6a
Duane Goodner <dmgoodner@gmail.com>
2024-08-28 11:56:38 -0600

    Restructure dirs for less nesting and simpler install with scikit-build. Local build works and all tests pass. Successfully installs w/ pip, but play_xiangqi broken b/c need to update binding module import statements in .py files

---
+++ _unused_setup.py
@@ -0,0 +1,18 @@
+from skbuild import setup
+from setuptools import find_packages
+
+
+setup(
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    include_package_data=True,
+    package_data={"": ["*.json"]},
+    # entry_points={
+    #     "console_scripts": [
+    #         "play_xiangqi = xiangqigame.__main__:main"
+    #     ]
+    # },
+    zip_safe=False,
+    cmake_install_dir="./src/xiangqigame_cpp",
+    cmake_source_dir="./src/xiangqigame_cpp"
+)
