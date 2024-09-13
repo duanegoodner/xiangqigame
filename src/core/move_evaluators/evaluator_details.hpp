e47a43f28fedeb7b9e9edbebce538664188d5d3a
Duane Goodner <dmgoodner@gmail.com>
2024-09-13 11:09:51 -0600

    Re-implement MinimaxResultType::kMin, and ::kMax and usage, this time with proper use in for loop indices to avoid seg fault

--- src/core/move_evaluators/evaluator_details.hpp
+++ src/core/move_evaluators/evaluator_details.hpp
@@ -17,17 +17,18 @@ struct RatedMove {
   Points_t rating;
 };
 
-enum MinimaxResultType : int {
+enum MinimaxResultType : size_t {
   kUnknown = 0,
   kFullyEvaluatedNode = 1,
   kStandardLeaf = 2,
   kEndGameLeaf = 3,
   kAlphaPrune = 4,
   kBetaPrune = 5,
-  kTrTableHit = 6
+  kTrTableHit = 6,
+  kMin = kUnknown,
+  kMax = kTrTableHit
 };
 const size_t kNumResultTypes{7};
-
 
 struct TranspositionTableEntry {
   int search_depth;
@@ -57,8 +58,8 @@ struct SearchSummary {
       : num_nodes{}
       , result_depth_counts{} {
     // reserve a "row" for each result type
-    result_depth_counts.reserve(kNumResultTypes);
-    for (auto idx = 0; idx < kNumResultTypes; idx++) {
+    result_depth_counts.reserve(MinimaxResultType::kMax);
+    for (auto idx = 0; idx <= MinimaxResultType::kMax; idx++) {
       // for each "row", create vector long enough to hold each possible search depth
       result_depth_counts.emplace_back(max_search_depth + 1, 0);
     }
