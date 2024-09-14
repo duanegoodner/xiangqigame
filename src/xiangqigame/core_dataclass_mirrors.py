d24869ce51eaafd155bcf013699c50eea98c3a45
Duane Goodner <dmgoodner@gmail.com>
2024-09-13 23:12:33 -0600

    add properties to SearchSummary and SearchSummaries with data configures for plotting

--- src/xiangqigame/core_dataclass_mirrors.py
+++ src/xiangqigame/core_dataclass_mirrors.py
@@ -1,28 +1,19 @@
 import datetime
-import numpy as np
-import pandas as pd
-
 from dataclasses import dataclass
 from typing import Dict, List
-from xiangqigame_core import (
-    BoardSpace,
-    GamePiece,
-    ExecutedMove,
-    Move,
-    PieceColor,
-    PieceType,
-    SearchSummary,
-    SearchSummaries,
-)
+
+import numpy as np
+import pandas as pd
+import xiangqigame_core as core
 
 
 @dataclass
 class GamePiece:
-    piece_color: PieceColor
-    piece_type: PieceType
+    piece_color: core.PieceColor
+    piece_type: core.PieceType
 
     @classmethod
-    def from_core_game_piece(cls, core_game_piece: GamePiece):
+    def from_core_game_piece(cls, core_game_piece: core.GamePiece):
         return cls(
             piece_color=core_game_piece.piece_color,
             piece_type=core_game_piece.piece_type,
@@ -35,7 +26,7 @@ class BoardSpace:
     file: int
 
     @classmethod
-    def from_core_board_space(cls, core_board_space: BoardSpace):
+    def from_core_board_space(cls, core_board_space: core.BoardSpace):
         return cls(rank=core_board_space.rank, file=core_board_space.file)
 
 
@@ -45,7 +36,7 @@ class Move:
     end: BoardSpace
 
     @classmethod
-    def from_core_move(cls, core_move: Move):
+    def from_core_move(cls, core_move: core.Move):
         start = BoardSpace.from_core_board_space(core_move.start)
         end = BoardSpace.from_core_board_space(core_move.end)
         return cls(start=start, end=end)
@@ -58,7 +49,7 @@ class ExecutedMove:
     spaces: Move
 
     @classmethod
-    def from_core_executed_move(cls, core_executed_move: ExecutedMove):
+    def from_core_executed_move(cls, core_executed_move: core.ExecutedMove):
         return cls(
             moving_piece=GamePiece.from_core_game_piece(
                 core_game_piece=core_executed_move.moving_piece
@@ -77,17 +68,16 @@ class SearchSummary:
     result_depth_counts: List[List[int]]
 
     @classmethod
-    def from_core_search_summary(cls, core_search_summary: SearchSummary):
+    def from_core_search_summary(cls, core_search_summary: core.SearchSummary):
         return cls(
             num_nodes=core_search_summary.num_nodes,
             time=core_search_summary.time,
             result_depth_counts=core_search_summary.result_depth_counts,
         )
-    
+
     @property
-    def flattened_rd_counts(self) -> pd.Series:
-        flattened_array = np.array(self.result_depth_counts).ravel()
-        return pd.Series(flattened_array)
+    def mean_time_per_node_ns(self) -> float:
+        return 1000000 * self.time.total_seconds() / self.num_nodes
 
 
 @dataclass
@@ -96,7 +86,9 @@ class SearchSummaries:
     extra_searches: Dict[int, SearchSummary]
 
     @classmethod
-    def from_core_search_summaries(cls, core_search_summaries: SearchSummaries):
+    def from_core_search_summaries(
+        cls, core_search_summaries: core.SearchSummaries
+    ):
         return cls(
             first_searches=[
                 SearchSummary.from_core_search_summary(item)
@@ -108,3 +100,45 @@ class SearchSummaries:
             },
         )
 
+    @property
+    def first_searches_by_type_and_depth(
+        self,
+    ) -> Dict[core.MinimaxResultType, List[List[int]]]:
+        result = {}
+
+        for enum_val in core.MinimaxResultType.__members__.values():
+            result[enum_val] = [
+                search_summary.result_depth_counts[int(enum_val)]
+                for search_summary in self.first_searches
+            ]
+
+        return result
+
+    @property
+    def first_searches_by_type(
+        self,
+    ) -> Dict[core.MinimaxResultType, List[int]]:
+        result = {}
+        for enum_val in core.MinimaxResultType.__members__.values():
+            result[enum_val] = [
+                sum(search_summary.result_depth_counts[int(enum_val)])
+                for search_summary in self.first_searches
+            ]
+        return result
+
+    @property
+    def first_searches_mean_time_per_node_ns(self) -> List[float]:
+        return [
+            search_summary.mean_time_per_node_ns
+            for search_summary in self.first_searches
+        ]
+
+    @property
+    def first_searches_total_nodes(self) -> List[int]:
+        return [
+            search_summary.num_nodes for search_summary in self.first_searches
+        ]
+
+    @property
+    def first_searches_time_s(self) -> List[float]:
+        return [search_summary.time.total_seconds() for search_summary in self.first_searches]
