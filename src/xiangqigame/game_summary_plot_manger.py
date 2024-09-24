b8161b314c0651b8254c98ae3642e0603a044408
Duane Goodner <dmgoodner@gmail.com>
2024-09-24 14:37:25 -0600

    fine tune plots with GameSummaryPlotManager and subclasses of GameSummaryPlotter

--- src/xiangqigame/game_summary_plot_manger.py
+++ src/xiangqigame/game_summary_plot_manger.py
@@ -3,18 +3,22 @@ from pathlib import Path
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.gridspec import GridSpec
-from numpy.ma.core import shape
 import xiangqigame_core as core
 
 from xiangqigame.game_summary import GameSummary
 from xiangqigame.game_summary_io import import_game_summary
-from xiangqigame.gist_gridspec import search_type_plots
+from xiangqigame.game_summary_plotters import (
+    SearchResultsByTypePlotter,
+    SearchTimePlotter,
+    EvalScorePlotter,
+)
 
 
 class GameSummaryPlotManger:
     def __init__(self, game_summary: GameSummary):
+        plt.style.use("bmh")
         self.game_summary = game_summary
-        self.fig = plt.figure(figsize=(12, 16))
+        self.fig = plt.figure(figsize=(12, 18))
         self.search_type_plots = np.empty(
             shape=(2, self.num_players_with_minimax_data), dtype=object
         )
@@ -28,7 +32,7 @@ class GameSummaryPlotManger:
             nrows=7,
             ncols=self.num_players_with_minimax_data,
             figure=self.fig,
-            height_ratios=[1, 1, 0.2, 1, 1, 0.2, 1],
+            height_ratios=[1, 1, 0.55, 1, 1, 0.45, 1],
         )
         self.build_layout()
 
@@ -49,39 +53,136 @@ class GameSummaryPlotManger:
     def build_layout(self):
         for gs_row in range(2):
             for gs_col in range(self.num_players_with_minimax_data):
-                self.search_type_plots[gs_row, gs_col] = (
+                self.search_type_plots[gs_row, gs_col] = self.fig.add_subplot(
+                    self.gs[gs_row, gs_col],
+                )
+
+        for gs_row in range(3, 5):
+            for gs_col in range(self.num_players_with_minimax_data):
+                self.search_time_plots[gs_row - 3, gs_col] = (
                     self.fig.add_subplot(
                         self.gs[gs_row, gs_col],
                     )
                 )
 
-        for gs_row in range(3, 5):
-            for gs_col in range(self.num_players_with_minimax_data):
-                self.search_time_plots[gs_row - 3, gs_col] = self.fig.add_subplot(
-                    self.gs[gs_row, gs_col],
-                )
-
         for gs_row in range(6, 7):
             for gs_col in range(self.num_players_with_minimax_data):
-                self.eval_score_plots[gs_row - 6, gs_col] = self.fig.add_subplot(
-                    self.gs[gs_row, gs_col],
+                self.eval_score_plots[gs_row - 6, gs_col] = (
+                    self.fig.add_subplot(
+                        self.gs[gs_row, gs_col],
+                    )
                 )
 
+        plt.subplots_adjust(
+            left=0.08,
+            right=0.73,
+            top=0.90,
+            bottom=0.05,
+            hspace=0.075,
+            wspace=0.075,
+        )
+
+        self.fig.text(
+            x=0.05, y=0.97, s=f"Summary plots for Game ID ____", fontsize=22
+        )
+
+        self.fig.text(
+            x=0.05,
+            y=0.93,
+            s="Minimax Node Counts by Search Result Type",
+            fontsize=18,
+        )
+
+        self.fig.text(
+            x=0.05,
+            y=0.57,
+            s="Move Selection Time Data",
+            fontsize=18,
+        )
+
+        self.fig.text(
+            x=0.05,
+            y=0.20,
+            s="Minimax Evaulation Scores",
+            fontsize=18,
+        )
+
+        self.fig.add_artist(
+            plt.Line2D(
+                [0.05, 0.95],
+                [0.955, 0.955],
+                transform=self.fig.transFigure,
+                color="black",
+            )
+        )
+
+        self.fig.add_artist(
+            plt.Line2D(
+                [0.05, 0.95],
+                [0.595, 0.595],
+                transform=self.fig.transFigure,
+                color="black",
+            )
+        )
+
+        self.fig.add_artist(
+            plt.Line2D(
+                [0.05, 0.95],
+                [0.23, 0.23],
+                transform=self.fig.transFigure,
+                color="black",
+            )
+        )
+
     def plot(self):
+        search_results_by_type_plotter = SearchResultsByTypePlotter(
+            axes=self.search_type_plots,
+            red_data=self.game_summary.get_player_summary(
+                player=core.PieceColor.kRed
+            ).first_searches_by_type,
+            black_data=self.game_summary.get_player_summary(
+                player=core.PieceColor.kBlk
+            ).first_searches_by_type,
+        )
+        search_results_by_type_plotter.plot()
+
+        search_time_plotter = SearchTimePlotter(
+            axes=self.search_time_plots,
+            red_data=self.game_summary.get_player_summary(
+                player=core.PieceColor.kRed
+            ).first_search_stats,
+            black_data=self.game_summary.get_player_summary(
+                player=core.PieceColor.kBlk
+            ).first_search_stats,
+        )
+        search_time_plotter.plot()
+
+        eval_score_plotter = EvalScorePlotter(
+            axes=self.eval_score_plots,
+            red_data=self.game_summary.get_player_summary(
+                player=core.PieceColor.kRed
+            ).first_search_stats,
+            black_data=self.game_summary.get_player_summary(
+                player=core.PieceColor.kBlk
+            ).first_search_stats,
+            add_plot_column_titles=False
+        )
+        eval_score_plotter.plot()
+
         plt.show()
 
 
 if __name__ == "__main__":
     game_summary_path_depth_6 = (
-            Path(__file__).parent.parent.parent
-            / "data"
-            / "20240923111251726641.json"
+        Path(__file__).parent.parent.parent
+        / "data"
+        / "20240923111251726641.json"
     )
 
     game_summary_path_depth_3 = (
-            Path(__file__).parent.parent.parent
-            / "data"
-            / "20240923104026139692.json"
+        Path(__file__).parent.parent.parent
+        / "data"
+        / "20240923104026139692.json"
     )
 
     my_game_summary = import_game_summary(path=game_summary_path_depth_6)
---
+++ src/xiangqigame/game_summary_plotters.py
@@ -0,0 +1,360 @@
+from abc import ABC, abstractmethod
+from typing import Dict, Tuple, cast, List
+
+import numpy as np
+import pandas as pd
+import xiangqigame_core as core
+from matplotlib import pyplot as plt
+
+from xiangqigame.core_dataclass_mirrors import PointsT
+
+
+class GameSummaryPlotter(ABC):
+    evaluating_player_line_colors = {
+        core.PieceColor.kRed: "red",
+        core.PieceColor.kBlk: "black",
+    }
+
+    player_text_labels = {
+        core.PieceColor.kRed: "Red Player",
+        core.PieceColor.kBlk: "Black Player",
+    }
+
+    non_evaluating_player_line_colors = {
+        core.PieceColor.kRed: "lightcoral",
+        core.PieceColor.kBlk: "darkgray",
+    }
+
+    def __init__(
+        self,
+        axes: np.ndarray,
+        y_labels: Tuple[str, ...],
+        log_scale_rows: int | Tuple[..., int] = tuple(),
+        red_data: pd.DataFrame = None,
+        black_data: pd.DataFrame = None,
+        add_plot_column_titles: bool = True,
+    ):
+        self.axes = axes
+        self.y_labels = y_labels
+        if type(log_scale_rows) == int:
+            log_scale_rows = (log_scale_rows,)
+        self.log_scale_rows = log_scale_rows
+        self._red_data = red_data
+        self._black_data = black_data
+        self._add_plot_column_titles = add_plot_column_titles
+        self.validate_dfs()
+        self.set_y_axes()
+        self.set_x_axes()
+
+    @property
+    def dfs(self) -> Dict[str, pd.DataFrame]:
+        return {
+            core.PieceColor.kRed.name: self._red_data,
+            core.PieceColor.kBlk.name: self._black_data,
+        }
+
+    def has_data(self, player: core.PieceColor) -> bool:
+        return self.dfs[player.name] is not None
+
+    @property
+    def num_players_with_data(self) -> int:
+        return sum(
+            [
+                int(self.has_data(core.PieceColor.kRed)),
+                int(self.has_data(core.PieceColor.kBlk)),
+            ]
+        )
+
+    def validate_dfs(self):
+        if self.num_players_with_data == 2:
+            assert (
+                self.dfs[core.PieceColor.kRed.name].columns
+                == self.dfs[core.PieceColor.kBlk.name].columns
+            ).all()
+
+    def set_y_axes(self):
+        for grid_row_idx, grid_row in enumerate(
+            self.axes[self.log_scale_rows, :]
+        ):
+            for ax in grid_row:
+                ax = cast(plt.Axes, ax)
+                ax.set_yscale("log")
+
+        for grid_row_idx, ax in enumerate(self.axes[:, 0]):
+            ax = cast(plt.Axes, ax)
+            ax.set_ylabel(self.y_labels[grid_row_idx], fontsize=14)
+
+    def set_x_axes(self):
+        for grid_row_idx, grid_row in enumerate(self.axes[0:-1, :]):
+            for ax in grid_row:
+                ax = cast(plt.Axes, ax)
+                ax.set_xticklabels([])
+
+        for grid_col_idx, ax in enumerate(self.axes[-1, :]):
+            ax = cast(plt.Axes, ax)
+            ax.set_xlabel("Game Move Number", fontsize=14)
+
+    @property
+    def player_plot_col(self) -> Dict[core.PieceColor, int]:
+        if self.num_players_with_data == 2:
+            return {core.PieceColor.kRed: 0, core.PieceColor.kBlk: 1}
+        elif self.has_data(player=core.PieceColor.kRed):
+            return {core.PieceColor.kRed: 0, core.PieceColor.kBlk: None}
+        elif self.has_data(player=core.PieceColor.kBlk):
+            return {core.PieceColor.kRed: None, core.PieceColor.kBlk: 1}
+
+    @property
+    def data_columns(self) -> pd.Index:
+        return [
+            item.columns for item in self.dfs.values() if item is not None
+        ][0]
+
+    @staticmethod
+    def match_y_limits(axes_row: np.ndarray):
+        y_limits_low = []
+        y_limits_high = []
+        for col_idx in range(axes_row.shape[0]):
+            cur_ax = cast(plt.Axes, axes_row[col_idx])
+            y_limits_low.append(cur_ax.get_ylim()[0])
+            y_limits_high.append(cur_ax.get_ylim()[1])
+        for col_idx in range(axes_row.shape[0]):
+            cur_ax = cast(plt.Axes, axes_row[col_idx])
+            cur_ax.set_ylim((min(y_limits_low), max(y_limits_high)))
+
+    def match_y_limits_all_rows(self):
+        for row_idx in range(self.axes.shape[0]):
+            self.match_y_limits(self.axes[row_idx, :])
+
+    @abstractmethod
+    def plot_data(self):
+        pass
+
+    def remove_second_plot_col_ylabels(self):
+        if self.axes.shape[1] == 2:
+            for idx, ax in enumerate(self.axes[:, 1]):
+                ax = cast(plt.Axes, ax)
+                ax.set_yticklabels([])
+
+    def set_plot_col_titles(self):
+        for idx, ax in enumerate(self.axes[0, :]):
+            ax = cast(plt.Axes, ax)
+            if idx == self.player_plot_col[core.PieceColor.kRed]:
+                ax.set_title(self.player_text_labels[core.PieceColor.kRed], fontsize=16)
+            if idx == self.player_plot_col[core.PieceColor.kBlk]:
+                ax.set_title(self.player_text_labels[core.PieceColor.kBlk], fontsize=16)
+
+    def plot(self):
+        self.plot_data()
+        self.remove_second_plot_col_ylabels()
+        if self._add_plot_column_titles:
+            self.set_plot_col_titles()
+        self.match_y_limits_all_rows()
+
+
+class SearchResultsByTypePlotter(GameSummaryPlotter):
+    def __init__(
+        self,
+        axes: np.ndarray,
+        y_labels: Tuple[str] = (
+            "Node Counts",
+            "Node Counts Log Scale",
+        ),
+        log_scale_rows: int | Tuple[int, int] = 1,
+        cmap_name: str = "Accent",
+        red_data: pd.DataFrame = None,
+        black_data: pd.DataFrame = None,
+        add_plot_column_titles: bool = True,
+    ):
+        super().__init__(
+            axes=axes,
+            y_labels=y_labels,
+            log_scale_rows=log_scale_rows,
+            red_data=red_data,
+            black_data=black_data,
+            add_plot_column_titles=add_plot_column_titles
+        )
+        self.cmap = plt.get_cmap(cmap_name)
+
+    @property
+    def data_column_colors(
+        self,
+    ) -> Dict[str, Tuple[float, float, float, float]]:
+        return {
+            cast(str, col_name): self.cmap(idx)
+            for idx, col_name in enumerate(self.data_columns)
+        }
+
+    def plot_search_results_by_type_stacked(
+        self, player: core.PieceColor, ax: plt.Axes
+    ):
+        df = self.dfs[player.name]
+        df_sorted = df[sorted(df.columns, key=lambda col: df[col].mean())]
+        sorted_colors = [
+            self.data_column_colors[col] for col in df_sorted.columns
+        ]
+        ax.stackplot(
+            df.index,
+            df_sorted.T,
+            labels=df_sorted.columns,
+            colors=sorted_colors,
+        )
+
+    def plot_player_data(self, player: core.PieceColor):
+        for plot_row in range(2):
+            plot_col = self.player_plot_col[player]
+            ax = cast(plt.Axes, self.axes[plot_row, plot_col])
+            self.plot_search_results_by_type_stacked(player=player, ax=ax)
+
+    def add_legend(self):
+        upper_right_plot = cast(plt.Axes, self.axes[0, -1])
+        handles = [
+            plt.Line2D(
+                xdata=[0],
+                ydata=[0],
+                color=self.data_column_colors[col],
+                lw=4,
+            )
+            for col in self.data_columns
+        ]
+        upper_right_plot.legend(
+            handles,
+            self.data_columns,
+            bbox_to_anchor=(1.05, 0.50),
+            fontsize=14,
+        )
+
+    def plot_data(self):
+
+        for player in [core.PieceColor.kRed, core.PieceColor.kBlk]:
+            if self.has_data(player=player):
+                self.plot_player_data(player=player)
+        self.add_legend()
+
+
+class SearchTimePlotter(GameSummaryPlotter):
+    search_stats_time_cols = ["search_time_s", "mean_time_per_node_ns"]
+
+    def __init__(
+        self,
+        axes: np.ndarray,
+        log_scale_rows: int | Tuple[..., int] = tuple(),
+        red_data: pd.DataFrame = None,
+        black_data: pd.DataFrame = None,
+        add_plot_column_titles: bool = True,
+    ):
+        super().__init__(
+            axes=axes,
+            y_labels=(
+                "Search Time (s)",
+                "Search Time per Node (ns)",
+            ),
+            log_scale_rows=log_scale_rows,
+            red_data=red_data,
+            black_data=black_data,
+            add_plot_column_titles=add_plot_column_titles
+        )
+
+    def plot_player_search_times(self, player: core.PieceColor):
+        df = self.dfs[player.name]
+        plot_col = self.player_plot_col[player]
+        for data_col_idx, data_col in enumerate(self.search_stats_time_cols):
+            ax = cast(plt.Axes, self.axes[data_col_idx, plot_col])
+            ax.plot(
+                df.index,
+                df[data_col],
+                color=self.evaluating_player_line_colors[player],
+            )
+
+    def plot_data(self):
+        for player in [core.PieceColor.kRed, core.PieceColor.kBlk]:
+            self.plot_player_search_times(player=player)
+
+
+class EvalScorePlotter(GameSummaryPlotter):
+    def __init__(
+        self,
+        axes: np.ndarray,
+        log_scale_rows: int | Tuple[..., int] = tuple(),
+        red_data: pd.DataFrame = None,
+        black_data: pd.DataFrame = None,
+        add_plot_column_titles: bool = True,
+    ):
+        super().__init__(
+            axes=axes,
+            y_labels=("Evaluation Score",),
+            log_scale_rows=log_scale_rows,
+            red_data=red_data,
+            black_data=black_data,
+            add_plot_column_titles=add_plot_column_titles
+        )
+
+    @property
+    def largest_magnitude_noninf_eval_score(self) -> int:
+        largest_score_magnitudes = []
+        inf_like_values = [np.iinfo(PointsT).max, np.iinfo(PointsT).min]
+        for df in self.dfs.values():
+            if df is not None:
+                assert df["eval_score"].dtype == PointsT
+                non_inf_like_values = df[
+                    ~df["eval_score"].isin(inf_like_values)
+                ]["eval_score"]
+                largest_score_magnitudes.append(
+                    np.abs(non_inf_like_values.min())
+                )
+                largest_score_magnitudes.append(
+                    np.abs(non_inf_like_values.max())
+                )
+        return max(largest_score_magnitudes)
+
+    @staticmethod
+    def symmetric_winsorize(
+        data: pd.Series, magnitude: int | float
+    ) -> pd.Series:
+        assert magnitude >= 0
+        return cast(
+            pd.Series, np.clip(data, a_min=-1 * magnitude, a_max=magnitude)
+        )
+
+    def plot_player_data(
+        self,
+        player: core.PieceColor,
+        ax: plt.Axes,
+        is_evaluating_player: bool = True,
+    ):
+        if self.has_data(player=player):
+            df = self.dfs[player.name]
+            if is_evaluating_player:
+                line_color = self.evaluating_player_line_colors[player]
+                line_style = "solid"
+            else:
+                line_color = self.non_evaluating_player_line_colors[player]
+                line_style = "dotted"
+            ax.plot(
+                df.index,
+                self.symmetric_winsorize(
+                    data=df["eval_score"],
+                    magnitude=1.5 * self.largest_magnitude_noninf_eval_score,
+                ),
+                color=line_color,
+                linestyle=line_style,
+                label=self.player_text_labels[player],
+            )
+            legend = ax.legend(loc="upper left", fontsize="12")
+            legend.get_frame().set_facecolor("white")
+
+    def plot_player_and_opponent_overlay(self, player: core.PieceColor):
+        plot_grid_col = self.player_plot_col[player]
+        ax = cast(plt.Axes, self.axes[0, plot_grid_col])
+        self.plot_player_data(player=player, ax=ax)
+
+        if self.has_data(player=core.opponent_of(player)):
+            self.plot_player_data(
+                player=core.opponent_of(player),
+                ax=ax,
+                is_evaluating_player=False,
+            )
+
+    def plot_data(self):
+        for player in [core.PieceColor.kRed, core.PieceColor.kBlk]:
+            if self.has_data(player=player):
+                self.plot_player_and_opponent_overlay(player=player)
