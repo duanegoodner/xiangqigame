316d8b0d1414ad75ab96eaf066668006c8a2f8ae
Duane Goodner <dmgoodner@gmail.com>
2024-09-24 23:49:30 -0600

    use alg_rank_modifier to correct move list used in test_scripted_game.py

--- tests/python/test_scripted_game.py
+++ tests/python/test_scripted_game.py
@@ -10,31 +10,31 @@ import unittest
 class ScriptedGameTests(unittest.TestCase):
 
     scripted_moves = [
-        "a7, a6",
         "a4, a5",
-        "a6, a5",
-        "b3, b5",
-        "a5, b5",
-        "c4, c5",
-        "b5, c5",
-        "c1, a3",
-        "e7, e6",
+        "a7, a6",
+        "a5, a6",
+        "b8, b6",
+        "a6, b6",
+        "c7, c6",
+        "b6, c6",
+        "c10, a8",
         "e4, e5",
-        "e6, e5",
-        "h1, g3",
-        "g10, e8",
-        "g3, e4",
-        "e5, e4",
-        "g1, i3",
-        "a10, a3",
-        "e1, e2",
-        "a3, e3",
-        "e2, f2",
-        "i10, i9",
-        "f1, e2",
-        "e3, e2",
-        "f2, f3",
-        "i9, f9",
+        "e7, e6",
+        "e5, e6",
+        "h10, g8",
+        "g1, e3",
+        "g8, e7",
+        "e6, e7",
+        "g10, i8",
+        "a1, a8",
+        "e10, e9",
+        "a8, e8",
+        "e9, f9",
+        "i1, i2",
+        "f10, e9",
+        "e8, e9",
+        "f9, f8",
+        "i2, f2",
     ]
 
     @staticmethod
@@ -42,9 +42,14 @@ class ScriptedGameTests(unittest.TestCase):
         red_moves, black_moves = move_list[::2], move_list[1::2]
         game_board = GameBoard()
         red_player = ScriptedPlayer(color=PieceColor.kRed, move_list=red_moves)
-        black_player = ScriptedPlayer(color=PieceColor.kBlk, move_list=black_moves)
+        black_player = ScriptedPlayer(
+            color=PieceColor.kBlk, move_list=black_moves
+        )
         game = Game(
-            players={PieceColor.kRed: red_player, PieceColor.kBlk: black_player},
+            players={
+                PieceColor.kRed: red_player,
+                PieceColor.kBlk: black_player,
+            },
             game_board=game_board,
         )
         game.play()
