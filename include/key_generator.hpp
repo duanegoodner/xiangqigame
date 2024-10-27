d2d0720641980bae6706ec9abad853a49d2d80fb
Duane Goodner <dmgoodner@gmail.com>
2024-10-27 11:28:29 -0600

    remove KeyGenerator to make sure always using KeyGeneratorNew

--- include/key_generator.hpp
+++ include/key_generator.hpp
@@ -49,47 +49,47 @@ void PrintHex(IntType num) {
   std::cout << std::dec << std::endl; // Reset to decimal output
 }
 
-//! Used by boardstate::ZobristKeys to generate hash keys. Supports 64-bit and 128-bit
-//! keys.
-class KeyGenerator {
-public:
-  KeyGenerator(unsigned long seed = std::random_device{}())
-      : gen_64_{seed} {}
+// //! Used by boardstate::ZobristKeys to generate hash keys. Supports 64-bit and 128-bit
+// //! keys.
+// class KeyGenerator {
+// public:
+//   KeyGenerator(unsigned long seed = std::random_device{}())
+//       : gen_64_{seed} {}
 
-  template <typename T>
-  T GenerateKey();
+//   template <typename T>
+//   T GenerateKey();
 
-  template <typename T>
-  void DisplayKey(T key);
+//   template <typename T>
+//   void DisplayKey(T key);
 
-private:
-  std::mt19937_64 gen_64_;
-};
+// private:
+//   std::mt19937_64 gen_64_;
+// };
 
-template <>
-inline uint64_t KeyGenerator::GenerateKey<uint64_t>() {
-  return gen_64_();
-}
+// template <>
+// inline uint64_t KeyGenerator::GenerateKey<uint64_t>() {
+//   return gen_64_();
+// }
 
-template <>
-inline void KeyGenerator::DisplayKey(uint64_t key) {
-  std::cout << "all bits = " << std::bitset<64>(key) << std::endl;
-}
+// template <>
+// inline void KeyGenerator::DisplayKey(uint64_t key) {
+//   std::cout << "all bits = " << std::bitset<64>(key) << std::endl;
+// }
 
-template <>
-inline __uint128_t KeyGenerator::GenerateKey<__uint128_t>() {
-  uint64_t high_bits = gen_64_();
-  uint64_t low_bits = gen_64_();
-  __uint128_t all_bits = ((__uint128_t)high_bits << 64) | low_bits;
-  return all_bits;
-}
+// template <>
+// inline __uint128_t KeyGenerator::GenerateKey<__uint128_t>() {
+//   uint64_t high_bits = gen_64_();
+//   uint64_t low_bits = gen_64_();
+//   __uint128_t all_bits = ((__uint128_t)high_bits << 64) | low_bits;
+//   return all_bits;
+// }
 
-template <>
-inline void KeyGenerator::DisplayKey(__uint128_t key) {
-  uint64_t hi_bits = static_cast<uint64_t>(key >> 64);
-  uint64_t lo_bits = static_cast<uint64_t>(key);
-  std::cout << "hi bits = " << std::bitset<64>(hi_bits) << std::endl;
-  std::cout << "lo bits = " << std::bitset<64>(lo_bits) << std::endl;
-}
+// template <>
+// inline void KeyGenerator::DisplayKey(__uint128_t key) {
+//   uint64_t hi_bits = static_cast<uint64_t>(key >> 64);
+//   uint64_t lo_bits = static_cast<uint64_t>(key);
+//   std::cout << "hi bits = " << std::bitset<64>(hi_bits) << std::endl;
+//   std::cout << "lo bits = " << std::bitset<64>(lo_bits) << std::endl;
+// }
 
 } // namespace boardstate
--- tests/core/key_generator_test.cpp
+++ tests/core/key_generator_test.cpp
@@ -3,25 +3,25 @@
 #include <key_generator.hpp>
 #include <random>
 
-class KeyGeneratorTest : public ::testing::Test {
-protected:
-};
+// class KeyGeneratorTest : public ::testing::Test {
+// protected:
+// };
 
-TEST_F(KeyGeneratorTest, InstantiateClass) {
-  boardstate::KeyGenerator key_generator;
-}
+// TEST_F(KeyGeneratorTest, InstantiateClass) {
+//   boardstate::KeyGenerator key_generator;
+// }
 
-TEST_F(KeyGeneratorTest, ClassGenerate64BitKey) {
-  boardstate::KeyGenerator key_generator;
-  auto key_64 = key_generator.GenerateKey<uint64_t>();
-  key_generator.DisplayKey(key_64);
-}
+// TEST_F(KeyGeneratorTest, ClassGenerate64BitKey) {
+//   boardstate::KeyGenerator key_generator;
+//   auto key_64 = key_generator.GenerateKey<uint64_t>();
+//   key_generator.DisplayKey(key_64);
+// }
 
-TEST_F(KeyGeneratorTest, ClassGenerate128BitKey) {
-  boardstate::KeyGenerator key_generator;
-  auto key_128 = key_generator.GenerateKey<__uint128_t>();
-  key_generator.DisplayKey(key_128);
-}
+// TEST_F(KeyGeneratorTest, ClassGenerate128BitKey) {
+//   boardstate::KeyGenerator key_generator;
+//   auto key_128 = key_generator.GenerateKey<__uint128_t>();
+//   key_generator.DisplayKey(key_128);
+// }
 
 
 class KeyGeneratorNewTest : public ::testing::Test {
