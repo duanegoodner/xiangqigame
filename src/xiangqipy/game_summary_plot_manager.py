43e08e965d7fa7cfdaa997c8f90e2249b72bd62d
Duane Goodner <dmgoodner@gmail.com>
2024-11-07 11:07:06 -0700

    remove zkeys_seed from Python-side PlayerSummary; plan to re-implement after deciding how to expose ZobristComponent seeds in bindings

--- src/xiangqipy/game_summary_plot_manager.py
+++ src/xiangqipy/game_summary_plot_manager.py
@@ -218,8 +218,8 @@ if __name__ == "__main__":
         Path(__file__).parent.parent.parent
         / "data"
         / "game_summaries"
-        / "20241107083108118185-test"
-        / "20241107083108118185.json"
+        / "20241107093914527094-test"
+        / "20241107093914527094.json"
     )
 
     my_game_summary = import_game_summary(path=my_game_summary_path)
