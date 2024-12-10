503593af50bb47084f8ca1b69201e6f260aa48de
Duane Goodner <dmgoodner@gmail.com>
2024-12-10 15:42:53 -0700

    more cmake cleanup

--- include/move_evaluator_human_for_concepts.hpp
+++ include/move_evaluator_human_for_concepts.hpp
@@ -1,3 +1,21 @@
 #pragma once
 
+#include <game_piece.hpp>
+#include <memory>
+#include <move_data_structs.hpp>
 
+namespace moveselection {
+class HumanMoveEvaluatorForConcepts {
+  gameboard::PieceColor evaluating_player_;
+
+public:
+  static std::unique_ptr<HumanMoveEvaluatorForConcepts> Create(
+      gameboard::PieceColor evaluating_player
+  );
+
+  gameboard::Move SelectMove(gameboard::MoveCollection &allowed_moves);
+
+private:
+  HumanMoveEvaluatorForConcepts(gameboard::PieceColor evaluating_player);
+};
+} // namespace moveselection
