4bcdc0d95476d44a01ddac85d564a50f474d3dcf
Duane Goodner <dmgoodner@gmail.com>
2024-12-11 13:47:48 -0700

    Change example move in MoveTranslatorTest to have rank and file change instead of only rank

--- tests/core/move_translator/move_translator_test.cpp
+++ tests/core/move_translator/move_translator_test.cpp
@@ -4,17 +4,17 @@
 
 class MoveTranslatorTest : public ::testing::Test {
 protected:
-  std::string valid_input_a_{"a4, a5"};
-  std::string invalid_input_b_{"a4a5"};
+  std::string valid_input_a_{"b1, c3"};
+  std::string invalid_input_b_{"b1c3"};
 
   std::istringstream valid_input_stream_a_;
   std::istringstream invalid_input_stream_b_;
 
-  std::vector<std::string> valid_parsed_input_a_{"a4", "a5"};
-  std::vector<std::string> invalid_parsed_input_b_{"a4a5"};
+  std::vector<std::string> valid_parsed_input_a_{"b1", "c3"};
+  std::vector<std::string> invalid_parsed_input_b_{"b1c3"};
 
-  gameboard::BoardSpace board_space_a_start_{6, 0};
-  gameboard::BoardSpace board_space_a_end_{5, 0};
+  gameboard::BoardSpace board_space_a_start_{9, 1};
+  gameboard::BoardSpace board_space_a_end_{7, 2};
   gameboard::Move move_a_{board_space_a_start_, board_space_a_end_};
 
   void SetUp() override {
@@ -34,12 +34,12 @@ TEST_F(MoveTranslatorTest, TestGetInput) {
 TEST_F(MoveTranslatorTest, TestParseInput) {
   auto valid_parsed_input = movetranslation::ParseInput(valid_input_a_);
   EXPECT_EQ(valid_parsed_input.size(), 2);
-  EXPECT_EQ(valid_parsed_input[0], "a4");
-  EXPECT_EQ(valid_parsed_input[1], "a5");
+  EXPECT_EQ(valid_parsed_input[0], "b1");
+  EXPECT_EQ(valid_parsed_input[1], "c3");
 
   auto invalid_parsed_input = movetranslation::ParseInput(invalid_input_b_);
   EXPECT_EQ(invalid_parsed_input.size(), 1);
-  EXPECT_EQ(invalid_parsed_input[0], "a4a5");
+  EXPECT_EQ(invalid_parsed_input[0], "b1c3");
 }
 
 TEST_F(MoveTranslatorTest, TestAlbebraicPairValidation) {
@@ -62,11 +62,11 @@ TEST_F(MoveTranslatorTest, TestAlbgebraicSpaceToBoardSpace) {
 TEST_F(MoveTranslatorTest, TestBoardSpaceToAlgebraicSpace) {
   auto computed_algebraic_space_a_start =
       movetranslation::BoardSpaceToAlgebraicSpace(board_space_a_start_);
-  EXPECT_EQ(computed_algebraic_space_a_start, "a4");
+  EXPECT_EQ(computed_algebraic_space_a_start, "b1");
 
   auto computed_algebraic_space_a_end =
       movetranslation::BoardSpaceToAlgebraicSpace(board_space_a_end_);
-  EXPECT_EQ(computed_algebraic_space_a_end, "a5");
+  EXPECT_EQ(computed_algebraic_space_a_end, "c3");
 }
 
 TEST_F(MoveTranslatorTest, TestConvertParsedInputToMove) {
