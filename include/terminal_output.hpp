00933f6c337169a740dfacc3debe5deaeae3b5c8
Duane Goodner <dmgoodner@gmail.com>
2024-12-17 12:16:47 -0700

    make terminaloutput conversion-to-string maps static const members of PlayerInfoReporter

--- include/terminal_output.hpp
+++ include/terminal_output.hpp
@@ -7,31 +7,6 @@
 
 namespace terminalout {
 
-const std::unordered_map<game::EvaluatorType, std::string> evaluator_to_player_type =
-    [] {
-      std::unordered_map<game::EvaluatorType, std::string> temp;
-      temp.insert(std::make_pair(game::EvaluatorType::kHuman, "Human"));
-      temp.insert(std::make_pair(game::EvaluatorType::kMinimax, "AI"));
-      temp.insert(std::make_pair(game::EvaluatorType::kRandom, "AI"));
-      return temp;
-    }();
-
-const std::unordered_map<game::EvaluatorType, std::string> evaluator_names = [] {
-  std::unordered_map<game::EvaluatorType, std::string> temp;
-  temp.insert(std::make_pair(game::EvaluatorType::kHuman, ""));
-  temp.insert(std::make_pair(game::EvaluatorType::kMinimax, "Minimax"));
-  temp.insert(std::make_pair(game::EvaluatorType::kRandom, "Random"));
-  return temp;
-}();
-
-const std::unordered_map<game::ZobristKeyType, std::string> key_type_strings = [] {
-  std::unordered_map<game::ZobristKeyType, std::string> temp;
-  temp.insert(std::make_pair(game::ZobristKeyType::k032, "32"));
-  temp.insert(std::make_pair(game::ZobristKeyType::k064, "64"));
-  temp.insert(std::make_pair(game::ZobristKeyType::k128, "128"));
-  return temp;
-}();
-
 class PlayerInfoReporter {
   game::PlayerSpec player_spec_;
 
@@ -40,6 +15,11 @@ public:
   void DisplaySummaryStr() { std::cout << SummaryStr() << std::endl; }
 
 private:
+  static const std::unordered_map<game::EvaluatorType, std::string>
+      evaluator_to_player_type_;
+  static const std::unordered_map<game::EvaluatorType, std::string> evaluator_names_;
+  static const std::unordered_map<game::ZobristKeyType, std::string> key_type_strings_;
+
   std::string PlayerTypeStr();
   std::string EvaluatorTypeStr();
   std::string SearchDepthStr();
--- src/core/terminal_output/terminal_output.cpp
+++ src/core/terminal_output/terminal_output.cpp
@@ -2,15 +2,37 @@
 
 namespace terminalout {
 
+// Definition of static constexpr maps
+const std::unordered_map<game::EvaluatorType, std::string>
+    PlayerInfoReporter::evaluator_to_player_type_ = {
+        {game::EvaluatorType::kHuman, "Human"},
+        {game::EvaluatorType::kMinimax, "AI"},
+        {game::EvaluatorType::kRandom, "AI"}
+};
+
+const std::unordered_map<game::EvaluatorType, std::string>
+    PlayerInfoReporter::evaluator_names_ = {
+        {game::EvaluatorType::kHuman, ""},
+        {game::EvaluatorType::kMinimax, "Minimax"},
+        {game::EvaluatorType::kRandom, "Random"}
+};
+
+const std::unordered_map<game::ZobristKeyType, std::string>
+    PlayerInfoReporter::key_type_strings_ = {
+        {game::ZobristKeyType::k032, "32"},
+        {game::ZobristKeyType::k064, "64"},
+        {game::ZobristKeyType::k128, "128"}
+};
+
 PlayerInfoReporter::PlayerInfoReporter(game::PlayerSpec player_spec)
     : player_spec_{player_spec} {}
 
 std::string PlayerInfoReporter::PlayerTypeStr() {
-  return "Player type = " + evaluator_to_player_type.at(player_spec_.evaluator_type);
+  return "Player type = " + evaluator_to_player_type_.at(player_spec_.evaluator_type);
 }
 
 std::string PlayerInfoReporter::EvaluatorTypeStr() {
-  return "Move Evaluator = " + evaluator_names.at(player_spec_.evaluator_type);
+  return "Move Evaluator = " + evaluator_names_.at(player_spec_.evaluator_type);
 }
 
 std::string PlayerInfoReporter::SearchDepthStr() {
@@ -19,7 +41,7 @@ std::string PlayerInfoReporter::SearchDepthStr() {
 
 std::string PlayerInfoReporter::ZobristKeySizeStr() {
   return "Zobrist Key Size = " +
-         key_type_strings.at(player_spec_.minimax_type_info.zobrist_key_type);
+         key_type_strings_.at(player_spec_.minimax_type_info.zobrist_key_type);
 }
 
 std::string PlayerInfoReporter::SummaryStr() {
