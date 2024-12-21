3a40c13e18ac8ec918c70cbcc9ed2d8b8c2aa548
Duane Goodner <dmgoodner@gmail.com>
2024-12-21 09:44:04 -0700

    rename utility_functs lib to utilities

--- cmake/add_subdirectories.cmake
+++ cmake/add_subdirectories.cmake
@@ -7,7 +7,7 @@ add_subdirectory(${SOURCE_CODE_DIR}/moveselection)
 add_subdirectory(${SOURCE_CODE_DIR}/movetranslation)
 add_subdirectory(${SOURCE_CODE_DIR}/piecepoints)
 add_subdirectory(${SOURCE_CODE_DIR}/terminalout)
-add_subdirectory(${SOURCE_CODE_DIR}/utility_functs)
+add_subdirectory(${SOURCE_CODE_DIR}/utilities)
 add_subdirectory(src/bindings)
 
 
