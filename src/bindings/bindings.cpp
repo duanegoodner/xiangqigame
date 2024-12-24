0aa8b3f83ede8c093090a7b88fdd04176fb5cf86
Duane Goodner <dmgoodner@gmail.com>
2024-12-23 21:39:49 -0700

    bindings: add some C++ classes needed to support GameRunner

--- src/bindings/bindings.cpp
+++ src/bindings/bindings.cpp
@@ -6,26 +6,27 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 
-#include <gameboard/board_data_structs.hpp>
+#include <boardstate/zobrist.hpp>
+#include <boardstate/zobrist_calculator_for_concepts.hpp>
 #include <boardstate/zobrist_factories.hpp>
-#include <moveselection/minimax_evaluator_factory.hpp>
+#include <boardstate/zobrist_for_concepts.hpp>
 #include <concepts/board_state_coordinator.hpp>
 #include <concepts/piece_value_provider.hpp>
 #include <concepts/space_info_provider.hpp>
+#include <game/game_data_structs.hpp>
+#include <gameboard/board_data_structs.hpp>
 #include <gameboard/game_board.hpp>
 #include <gameboard/game_board_for_concepts.hpp>
-#include <utilities/integer_types.hpp>
+#include <moveselection/minimax_evaluator_factory.hpp>
+#include <moveselection/move_evaluator_minimax_for_concepts.hpp>
 #include <moveselection/move_evaluator_random_for_concepts.hpp>
 #include <moveselection/move_evaluators.hpp>
-#include <moveselection/move_evaluator_minimax_for_concepts.hpp>
 #include <piecepoints/base_position_offset.hpp>
 #include <piecepoints/piece_position_points.hpp>
 #include <piecepoints/piece_position_points_for_concepts.hpp>
 #include <random>
 #include <string>
-#include <boardstate/zobrist.hpp>
-#include <boardstate/zobrist_calculator_for_concepts.hpp>
-#include <boardstate/zobrist_for_concepts.hpp>
+#include <utilities/integer_types.hpp>
 
 namespace py = pybind11;
 using namespace py::literals;
@@ -97,101 +98,41 @@ void bind_minimax_move_evaluator(py::module_ &m, const std::string &class_name) 
       );
 }
 
-// // template <
-// //     BoardStateCoordinatorConcept H,
-// //     SpaceInfoProviderConcept G,
-// //     PieceValueProviderConcept P>
-// template <typename KeyType, size_t NumConfKeys>
-// void bind_minimax_move_evaluator_for_concepts(
-//     py::module_ &m,
-//     const std::string &class_name
-// ) {
-//   using CalculatorType = ZobristCalculatorForConcepts<KeyType>;
-//   using ComponentType = ZobristComponentForConcepts<CalculatorType, NumConfKeys>;
-//   using CoordinatorType = ZobristCoordinatorForConcepts<ComponentType>;
-//   py::class_<moveselection::MinimaxMoveEvaluatorForConcepts<
-//       CoordinatorType,
-//       GameBoardForConcepts,
-//       PiecePositionPointsForConcepts>>(m, class_name.c_str())
-//       .def(
-//           py::init<
-//               PieceColor,
-//               DepthType,
-//               std::shared_ptr<GameBoardForConcepts>,
-//               std::shared_ptr<PiecePositionPointsForConcepts>,
-//               std::shared_ptr<CoordinatorType>>(),
-//           "evaluating_player"_a,
-//           "search_depth"_a,
-//           "game_board"_a,
-//           "game_position_points"_a,
-//           "hash_calculator"_a
-//       )
-//       .def(
-//           "select_move",
-//           &moveselection::MinimaxMoveEvaluatorForConcepts<
-//               CoordinatorType,
-//               GameBoardForConcepts,
-//               PiecePositionPointsForConcepts>::SelectMove,
-//           "allowed_moves"_a
-//       )
-//       .def(
-//           "search_summaries",
-//           &moveselection::MinimaxMoveEvaluatorForConcepts<
-//               CoordinatorType,
-//               GameBoardForConcepts,
-//               PiecePositionPointsForConcepts>::search_summaries
-//       )
-//       .def(
-//           "search_depth",
-//           &moveselection::MinimaxMoveEvaluatorForConcepts<
-//               CoordinatorType,
-//               GameBoardForConcepts,
-//               PiecePositionPointsForConcepts>::search_depth
-//       )
-//       .def(
-//           "zobrist_key_size_bits",
-//           &moveselection::MinimaxMoveEvaluatorForConcepts<
-//               CoordinatorType,
-//               GameBoardForConcepts,
-//               PiecePositionPointsForConcepts>::KeySizeBits
-//       )
-//       .def_property_readonly(
-//           "zkeys_seed",
-//           &moveselection::MinimaxMoveEvaluatorForConcepts<
-//               CoordinatorType,
-//               GameBoardForConcepts,
-//               PiecePositionPointsForConcepts>::zkeys_seed
-//       )
-//       .def_property_readonly(
-//           "board_state_hex_str",
-//           &moveselection::MinimaxMoveEvaluatorForConcepts<
-//               CoordinatorType,
-//               GameBoardForConcepts,
-//               PiecePositionPointsForConcepts>::board_state_hex_str
-//       );
-// }
-
-// template <typename KeyType, size_t NumConfKeys>
-// void bind_minimax_move_evaluator_factory(py::module_ &m, const std::string &class_name) {
-//   py::class_<moveselection::MinimaxMoveEvaluatorFactory<
-//       KeyType,
-//       NumConfKeys,
-//       gameboard::GameBoardForConcepts>>(m, class_name.c_str())
-//       .def(py::init<>())
-//       .def(
-//           "create",
-//           &moveselection::MinimaxMoveEvaluatorFactory<
-//               KeyType,
-//               NumConfKeys,
-//               GameBoardForConcepts>::Create,
-//           py::arg("game_board"),
-//           py::arg("evaluating_player"),
-//           py::arg("search_depth"),
-//           py::arg("json_file") = kICGABPOPath
-//       );
-// }
-
 PYBIND11_MODULE(xiangqi_bindings, m) {
+  py::enum_<game::GameState>(m, "GameState")
+      .value("kUnfinished", game::kUnfinished)
+      .value("kDraw", game::kDraw)
+      .value("kRedWon", game::kRedWon)
+      .value("kBlkWon", game::kBlkWon);
+
+  py::enum_<game::EvaluatorType>(m, "EvaluatorType")
+      .value("kRandom", game::kRandom)
+      .value("kMinimax", game::kMinimax)
+      .value("kHuman", game::kHuman);
+
+  py::enum_<game::ZobristKeyType>(m, "ZobristKeyType")
+      .value("kNoKey", game::kNoKey)
+      .value("k032Bitkey", game::k032BitKey)
+      .value("k064BitKey", game::k064BitKey)
+      .value("k128BitKey", game::k128BitKey);
+
+  py::enum_<game::ZobristCalculatorCount>(m, "ZobristCalculatorCount")
+      .value("kNoZCalcs", game::kNoZCalcs)
+      .value("kOneZCalc", game::kOneZCalc)
+      .value("kTwoZCalc", game::kTwoZCalc);
+
+  py::class_<game::PlayerSpec>(m, "PlayerSpec")
+      .def_readonly("color", &game::PlayerSpec::color)
+      .def_readonly("zobrist_key_type", &game::PlayerSpec::zobrist_key_type)
+      .def_readonly(
+          "zobrist_calculator_count",
+          &game::PlayerSpec::zobrist_calculator_count
+      )
+      .def_readonly("minimax_search_depth", &game::PlayerSpec::minimax_search_depth);
+
+  py::class_<game::GameSummary>(m, "GameSummary")
+      .def_readonly("game_id", &game::GameSummary::game_id)
+      .def_readonly("game_state", &game::GameSummary::game_state);
 
   // Define key types and their corresponding names as a tuple
   const std::tuple<std::string, size_t> key_types[] = {
@@ -299,30 +240,30 @@ PYBIND11_MODULE(xiangqi_bindings, m) {
       .def_property_readonly("is_draw", &GameBoard::IsDraw)
       .def("GetColor", &GameBoard::GetColor, "space"_a);
 
-//   py::class_<GameBoardForConcepts>(m, "GameBoardForConcepts")
-//       //   .def(py::init<>())
-//       .def("map", &GameBoardForConcepts::map)
-//       .def("ExecuteMove", &GameBoardForConcepts::ExecuteMove, "move"_a)
-//       .def("UndoMove", &GameBoardForConcepts::UndoMove, "executed_move"_a)
-//       .def(
-//           "GetAllSpacesOccupiedBy",
-//           &GameBoardForConcepts::GetAllSpacesOccupiedBy,
-//           "color"_a
-//       )
-//       .def("CalcFinalMovesOf", &GameBoardForConcepts::CalcFinalMovesOf, "color"_a)
-//       .def("IsInCheck", &GameBoardForConcepts::IsInCheck, "color"_a)
-//       .def("GetType", &GameBoardForConcepts::GetType, "space"_a)
-//       .def_property_readonly("move_log", &GameBoardForConcepts::move_log)
-//       .def_property_readonly("is_draw", &GameBoardForConcepts::IsDraw)
-//       .def("GetColor", &GameBoardForConcepts::GetColor, "space"_a);
+  //   py::class_<GameBoardForConcepts>(m, "GameBoardForConcepts")
+  //       //   .def(py::init<>())
+  //       .def("map", &GameBoardForConcepts::map)
+  //       .def("ExecuteMove", &GameBoardForConcepts::ExecuteMove, "move"_a)
+  //       .def("UndoMove", &GameBoardForConcepts::UndoMove, "executed_move"_a)
+  //       .def(
+  //           "GetAllSpacesOccupiedBy",
+  //           &GameBoardForConcepts::GetAllSpacesOccupiedBy,
+  //           "color"_a
+  //       )
+  //       .def("CalcFinalMovesOf", &GameBoardForConcepts::CalcFinalMovesOf, "color"_a)
+  //       .def("IsInCheck", &GameBoardForConcepts::IsInCheck, "color"_a)
+  //       .def("GetType", &GameBoardForConcepts::GetType, "space"_a)
+  //       .def_property_readonly("move_log", &GameBoardForConcepts::move_log)
+  //       .def_property_readonly("is_draw", &GameBoardForConcepts::IsDraw)
+  //       .def("GetColor", &GameBoardForConcepts::GetColor, "space"_a);
 
-//   py::class_<GameBoardFactory>(m, "GameBoardFactory")
-//       .def(py::init<>())
-//       .def(
-//           "create",
-//           &GameBoardFactory::Create,
-//           py::arg("starting_board") = kStandardInitialBoard
-//       );
+  //   py::class_<GameBoardFactory>(m, "GameBoardFactory")
+  //       .def(py::init<>())
+  //       .def(
+  //           "create",
+  //           &GameBoardFactory::Create,
+  //           py::arg("starting_board") = kStandardInitialBoard
+  //       );
 
   m.def("opponent_of", &opponent_of);
 
@@ -333,23 +274,24 @@ PYBIND11_MODULE(xiangqi_bindings, m) {
           &moveselection::RandomMoveEvaluator<GameBoard>::SelectMove,
           "allowed_moves"_a
       );
-//   py::class_<moveselection::RandomMoveEvaluatorForConcepts>(
-//       m,
-//       "RandomMoveEvaluatorForConcepts"
-//   )
-//       .def(
-//           "select_move",
-//           &moveselection::RandomMoveEvaluatorForConcepts::SelectMove,
-//           "allowed_moves"_a
-//       );
+  //   py::class_<moveselection::RandomMoveEvaluatorForConcepts>(
+  //       m,
+  //       "RandomMoveEvaluatorForConcepts"
+  //   )
+  //       .def(
+  //           "select_move",
+  //           &moveselection::RandomMoveEvaluatorForConcepts::SelectMove,
+  //           "allowed_moves"_a
+  //       );
 
-//   py::class_<moveselection::RandomMoveEvaluatorFactory>(m, "RandomMoveEvaluatorFactory")
-//       .def(py::init<>())
-//       .def(
-//           "create",
-//           &moveselection::RandomMoveEvaluatorFactory::Create,
-//           "evaluating_player"_a
-//       );
+  //   py::class_<moveselection::RandomMoveEvaluatorFactory>(m,
+  //   "RandomMoveEvaluatorFactory")
+  //       .def(py::init<>())
+  //       .def(
+  //           "create",
+  //           &moveselection::RandomMoveEvaluatorFactory::Create,
+  //           "evaluating_player"_a
+  //       );
 
   py::class_<moveselection::SearchSummary>(m, "SearchSummary")
       .def_property_readonly(
@@ -408,47 +350,48 @@ PYBIND11_MODULE(xiangqi_bindings, m) {
   bind_minimax_move_evaluator<uint64_t, 1>(m, "MinimaxMoveEvaluator64Dual");
   bind_minimax_move_evaluator<__uint128_t, 1>(m, "MinimaxMoveEvaluator128Dual");
 
-//   bind_minimax_move_evaluator_for_concepts<uint32_t, 0>(
-//       m,
-//       "MinimaxMoveEvaluatorForConcepts32"
-//   );
-//   bind_minimax_move_evaluator_for_concepts<uint64_t, 0>(
-//       m,
-//       "MinimaxMoveEvaluatorForConcepts64"
-//   );
-//   bind_minimax_move_evaluator_for_concepts<__uint128_t, 0>(
-//       m,
-//       "MinimaxMoveEvaluatorForConcepts128"
-//   );
-//   bind_minimax_move_evaluator_for_concepts<uint32_t, 1>(
-//       m,
-//       "MinimaxMoveEvaluatorForConcepts32Dual"
-//   );
-//   bind_minimax_move_evaluator_for_concepts<uint64_t, 1>(
-//       m,
-//       "MinimaxMoveEvaluatorForConcepts64Dual"
-//   );
-//   bind_minimax_move_evaluator_for_concepts<__uint128_t, 1>(
-//       m,
-//       "MinimaxMoveEvaluatorForConcepts128Dual"
-//   );
+  //   bind_minimax_move_evaluator_for_concepts<uint32_t, 0>(
+  //       m,
+  //       "MinimaxMoveEvaluatorForConcepts32"
+  //   );
+  //   bind_minimax_move_evaluator_for_concepts<uint64_t, 0>(
+  //       m,
+  //       "MinimaxMoveEvaluatorForConcepts64"
+  //   );
+  //   bind_minimax_move_evaluator_for_concepts<__uint128_t, 0>(
+  //       m,
+  //       "MinimaxMoveEvaluatorForConcepts128"
+  //   );
+  //   bind_minimax_move_evaluator_for_concepts<uint32_t, 1>(
+  //       m,
+  //       "MinimaxMoveEvaluatorForConcepts32Dual"
+  //   );
+  //   bind_minimax_move_evaluator_for_concepts<uint64_t, 1>(
+  //       m,
+  //       "MinimaxMoveEvaluatorForConcepts64Dual"
+  //   );
+  //   bind_minimax_move_evaluator_for_concepts<__uint128_t, 1>(
+  //       m,
+  //       "MinimaxMoveEvaluatorForConcepts128Dual"
+  //   );
 
-//   bind_minimax_move_evaluator_factory<uint32_t, 0>(m, "MinimaxMoveEvaluatorFactory32");
-//   bind_minimax_move_evaluator_factory<uint64_t, 0>(m, "MinimaxMoveEvaluatorFactory64");
-//   bind_minimax_move_evaluator_factory<__uint128_t, 0>(
-//       m,
-//       "MinimaxMoveEvaluatorFactory128"
-//   );
-//   bind_minimax_move_evaluator_factory<uint32_t, 1>(
-//       m,
-//       "MinimaxMoveEvaluatorFactory32Dual"
-//   );
-//   bind_minimax_move_evaluator_factory<uint64_t, 1>(
-//       m,
-//       "MinimaxMoveEvaluatorFactory64Dual"
-//   );
-//   bind_minimax_move_evaluator_factory<__uint128_t, 1>(
-//       m,
-//       "MinimaxMoveEvaluatorFactory128Dual"
-//   );
+  //   bind_minimax_move_evaluator_factory<uint32_t, 0>(m,
+  //   "MinimaxMoveEvaluatorFactory32"); bind_minimax_move_evaluator_factory<uint64_t,
+  //   0>(m, "MinimaxMoveEvaluatorFactory64");
+  //   bind_minimax_move_evaluator_factory<__uint128_t, 0>(
+  //       m,
+  //       "MinimaxMoveEvaluatorFactory128"
+  //   );
+  //   bind_minimax_move_evaluator_factory<uint32_t, 1>(
+  //       m,
+  //       "MinimaxMoveEvaluatorFactory32Dual"
+  //   );
+  //   bind_minimax_move_evaluator_factory<uint64_t, 1>(
+  //       m,
+  //       "MinimaxMoveEvaluatorFactory64Dual"
+  //   );
+  //   bind_minimax_move_evaluator_factory<__uint128_t, 1>(
+  //       m,
+  //       "MinimaxMoveEvaluatorFactory128Dual"
+  //   );
 }
